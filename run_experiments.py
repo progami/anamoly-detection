66116b69ce0f8f7d08deceb76fe35b181abf5faf
MaherDissem <maherdissem26@gmail.com>
2024-02-05 14:27:00 -0500

    optimization

--- run_experiments.py
+++ run_experiments.py
@@ -3,41 +3,44 @@
     note: need to generate dataset first, run `./prepare_data.py`
 """
 import os
-import multiprocessing as mp
-from main import main
+import concurrent.futures
 
-# environment variables
-gpu_ids = [0, 1, 2, 3]
-nbr_workers = len(gpu_ids)
+from main import main as run_experiment
+
+# Environment variables
+gpu_ids = [0, 1, 2, 3] # List of GPU IDs to use
+job_per_gpu = 2        # Number of jobs to run on each GPU
 
 # experiment variables
 dataset_names = ["IHAMPS1", "IMONTR17", "IMONTR172", "IQUEBECM21", "IWESTM40"] 
 feature_names = ["Temperature_C", "Humidity_%", "Pressure_hPa"]
 
 if __name__ == "__main__":
-    experiment_args = []
-    i = 0
-    for dataset in dataset_names:
-        for feat in feature_names:
-            train_path = f"dataset/synth_ts_data/train_{dataset}_{feat}.csv"
-            test_path = f"dataset/synth_ts_data/test_{dataset}_{feat}.csv"
-            output_file = f"results/{dataset}_{feat}.txt"
-            os.makedirs("results", exist_ok=True)
-            gpu_id = gpu_ids[i % len(gpu_ids)] # Assign GPUs in a round-robin fashion
-            experiment_args.append((train_path, test_path, output_file, gpu_id))
-            i += 1
-    
-    # divide the number of experiments by the max number of parallel processes
-    chunks = [experiment_args[i:i+nbr_workers] for i in range(0, len(experiment_args), nbr_workers)]
+    os.makedirs("results", exist_ok=True)
+    n_workers = len(gpu_ids) * job_per_gpu
 
-    for chunk in chunks:
-        processes = []
-        for args in chunk:
-            process = mp.Process(target=main, args=args)
-            processes.append(process)
-            process.start()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=n_workers) as executor:
+        futures = {}
+        i = 0
+        for dataset in dataset_names:
+            for feat in feature_names:
+                # Set up the experiment parameters
+                train_path = f"dataset/synth_ts_data/train_{dataset}_{feat}.csv"
+                test_path = f"dataset/synth_ts_data/test_{dataset}_{feat}.csv"
+                output_file = f"results/{dataset}_{feat}.txt"
+                gpu_id = gpu_ids[i % len(gpu_ids)] # Assign GPUs in a round-robin fashion
+                i += 1
 
-        for process in processes:
-            process.join()
+                # Submit an experiment for execution and store the future object
+                futures[executor.submit(run_experiment, train_path, test_path, output_file, gpu_id)] = (train_path, test_path, output_file, gpu_id)
 
-    print("Done!")
+        # Iterate over completed futures as they become available
+        for completed_future in concurrent.futures.as_completed(futures):
+            train_path, test_path, output_file, gpu_id = futures[completed_future]
+            try:
+                completed_future.result() # Blocked until result is ready
+            except Exception as e:
+                print(f"Training failed for dataset: {train_path} with error: {e}")
+
+        print("Multi-processes finished")
+
